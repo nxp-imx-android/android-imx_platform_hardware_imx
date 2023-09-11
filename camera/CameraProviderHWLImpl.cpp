04baa03a4882d6d381d72cf9d96b6571c70a7e33
Anle Pan <anle.pan@nxp.com>
2023-09-11 14:55:58 +0000

    MA-21646 Fix Coverity issues for nxp camera
    fix below coverity issues in nxp camera hal,
    type:COPY_INSTEAD_OF_MOVE
    CID:
    33970081 33970054 33970036 33970024 33970004
    33969938 33969932 33969924 33969844
    
    Change-Id: I2efd181622301c902ec2dd22ccfccb6f3b848d17
    Signed-off-by: Anle Pan <anle.pan@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/22686
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- camera/CameraProviderHWLImpl.cpp
+++ camera/CameraProviderHWLImpl.cpp
@@ -481,7 +481,7 @@ status_t CameraProviderHwlImpl::CreateCameraDeviceHwl(
         physicalIds.push_back(0);
     }
 
-    *camera_device_hwl = CameraDeviceHwlImpl::Create(camera_id, devPaths, physicalIds,
+    *camera_device_hwl = CameraDeviceHwlImpl::Create(camera_id, std::move(devPaths), std::move(physicalIds),
         mCameraDef.cam_blit_copy_hw, mCameraDef.cam_blit_csc_hw, mCameraDef.jpeg_hw.c_str(),
         mCameraDef.mUseCpuEncoder, &cam_metadata, std::move(target_physical_devices), mCallback);
 
